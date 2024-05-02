25e374bb3c19a1cce9d5f3a9d2539885da342dcb
Thiago Morano <thiago.morano159@gmail.com>
2024-05-02 20:31:31 +0200

    add greet based on language

--- 00-hello-world/hello_test.go
+++ 00-hello-world/hello_test.go
@@ -5,15 +5,25 @@ import "testing"
 func TestHello(t *testing.T) {
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Lena")
+		got := Hello("Lena", "")
 		want := "Hello, Lena"
 
 		assetCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
+		assetCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Lena", "Spanish")
+		want := "Hola, Lena"
+		assetCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Thomas", "French")
+		want := "Bonjour, Thomas"
 		assetCorrectMessage(t, got, want)
 	})
 }
