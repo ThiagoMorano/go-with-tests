cf8cc97a340eca253bab465c8a51d748f49574d3
Thiago Morano <thiago.morano159@gmail.com>
2024-05-02 19:54:51 +0200

    add hello to given name

--- 00-hello-world/hello.go
+++ 00-hello-world/hello.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!")
+	fmt.Println(Hello("world"))
+}
+
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
