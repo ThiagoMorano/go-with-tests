4158b810674c34ee92202fe3dc12b22881cb74be
Thiago Morano <thiago.morano159@gmail.com>
2024-05-09 12:20:43 +0200

    add dictionary

---
+++ 06-maps/dictionary_test.go
@@ -0,0 +1,41 @@
+package main
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertString(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := ErrNotFound
+
+		if err == nil {
+			t.Fatal("got no error but expected one")
+		}
+
+		assertError(t, err, want)
+	})
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
+}
+
+func assertString(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
