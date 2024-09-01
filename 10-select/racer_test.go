82c9e6dcc30565babc3f98dabb0e031d72c10e7a
Thiago Morano <thiago.morano159@gmail.com>
2024-09-01 18:50:38 +0200

    add chapter 10 on keyword select

---
+++ 10-select/racer_test.go
@@ -0,0 +1,52 @@
+package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+
+	t.Run("compares speed of servers, returning the url of the fastest", func(t *testing.T) {
+		slowServer := makeDelayedServer(10 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
+
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+
+		want := fastURL
+		got, err := Racer(slowURL, fastURL)
+
+		if err != nil {
+			t.Fatalf("did not expect an error but got one %v", err)
+		}
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't response within 10s", func(t *testing.T) {
+		serverA := makeDelayedServer(11 * time.Millisecond)
+
+		defer serverA.Close()
+
+		_, err := ConfigurableRacer(serverA.URL, serverA.URL, 10*time.Millisecond)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
